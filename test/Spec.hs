cdb3f9ac2a6b29aa80d6a6bc1174ee7ff8a803ea
Chandra Koduru <kscraja@gmail.com>
2017-08-04 18:22:24 +0530

    adding package.yaml and the generated cabal package

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+
+import Test.QuickCheck
+
+import Lib
+
+prop_revapp :: [Int] -> [Int] -> Bool
+prop_revapp xs ys = reverse (xs ++ ys) == reverse ys ++ reverse xs
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_revapp
+  someFunc
+
